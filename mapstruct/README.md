393fec28c55fc73949a433870ae6656a907aa4c1
Gonzalo Moralejo <gonzalo.moralejo@globant.com>
2019-10-22 19:37:15 -0700

    remove some projects

--- mapstruct/README.md
+++
@@ -1,8 +0,0 @@
-## MapStruct
-
-This module contains articles about MapStruct.
-
-###Relevant Articles:
-- [Quick Guide to MapStruct](https://www.baeldung.com/mapstruct)
-- [Custom Mapper with MapStruct](https://www.baeldung.com/mapstruct-custom-mapper)
-
--- spring-jersey/src/main/java/com/baeldung/client/rest/RestClient.java
+++ spring-jersey/src/main/java/com/baeldung/client/rest/RestClient.java
@@ -13,10 +13,16 @@ public class RestClient {
     private static final String REST_URI = "http://localhost:8082/spring-jersey/resources/employees";
     private Client client = ClientBuilder.newClient();
 
+    /**
+     * Jersey client post.
+     */
     public Response createJsonEmployee(Employee emp) {
         return client.target(REST_URI).request(MediaType.APPLICATION_JSON).post(Entity.entity(emp, MediaType.APPLICATION_JSON));
     }
 
+    /**
+     * Jersey client get.
+     */
     public Employee getJsonEmployee(int id) {
         return client.target(REST_URI).path(String.valueOf(id)).request(MediaType.APPLICATION_JSON).get(Employee.class);
     }
